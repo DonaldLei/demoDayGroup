5b941a28b7ff823e7139433a3d786086a3894e92
ASCkenth <kehe@stu.allstarcode.org>
2023-08-14 19:14:21 -0400

    added battle page

---
+++ rpg-game/rpg-game-battle/rpg.js
@@ -0,0 +1,12 @@
+function setup(){
+	let cnv=createCanvas(600,500);
+	cnv.parent('game-div');
+	background(0);
+}
+//https://stackoverflow.com/questions/35660240/how-to-put-p5-js-canvas-in-a-html-div
+
+function draw(){
+	// fill(random(0,255),random(0,255),random(0,255));
+	ellipse(200,200,50);
+	
+}
